55a4ed7e6a9b4919d9a11d4b1e619d94fc9f4d25
FcoEvanderson <f.evanderson13@gmail.com>
2024-06-17 22:31:58 -0300

    moviesite

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
+import { useEffect, useState } from "react";
 import Results from "@/components/Results";
 import GenreButton from "@/components/GenreButton";
-import { useEffect, useState } from "react";
 
 const API_KEY = process.env.API_KEY;
 
-export default async function Home({ searchParams }) {
+export default function Home({ searchParams }) {
   const [results, setResults] = useState([]);
   const [genresList, setGenresList] = useState([]);
   const [currentPage, setCurrentPage] = useState(parseInt(searchParams.page) || 1);
@@ -14,28 +14,28 @@ export default async function Home({ searchParams }) {
 
   const genre = searchParams.genre || 'fetchTrending';
 
-  useEffect(() =>{
+  useEffect(() => {
     const fetchMovies = async () => {
       const today = new Date().toISOString().split('T')[0];
       const year = new Date().getFullYear();
-      const firstDayOfYear = `${year}-01-01`; 
-      const lastDayOfYear = `${year}-12-31`; 
+      const firstDayOfYear = `${year}-01-01`;
+      const lastDayOfYear = `${year}-12-31`;
 
       const discoverUrl = genre === 'fetchTrending'
-        ? `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&primary_release_date.gte=${firstDayOfYear}&primary_release_date.lte=${lastDayOfYear}&page=1`
-        : `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&primary_release_date.lte=${today}&with_genres=${genre}&page=1`;
-        
-        try{
-          const response = await fetch(discoverUrl, { next: { revalidate: 10000 } });
-          const data = await response.json();
-          if (!response.ok) {
-            throw new Error("Failed to fetch data")
-          }
-          setResults(data.results);
-          setTotalPages(data.total_pages);
-        } catch (error){
-          console.error('Error Fetching movies: ', error);
+        ? `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&primary_release_date.gte=${firstDayOfYear}&primary_release_date.lte=${lastDayOfYear}&page=${currentPage}`
+        : `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&primary_release_date.lte=${today}&with_genres=${genre}&page=${currentPage}`;
+
+      try {
+        const response = await fetch(discoverUrl);
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
         }
+        setResults(data.results || []);
+        setTotalPages(data.total_pages || 0);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
     };
 
     fetchMovies();
@@ -49,14 +49,14 @@ export default async function Home({ searchParams }) {
         );
         const genresData = await genresResponse.json();
         setGenresList(genresData.genres || []);
-      } catch (error){
-        console.error('Error fetching genres', error);
-        setGenresList([]);
+      } catch (error) {
+        console.error("Error fetching genres:", error);
+        setGenresList([]); // Definir uma lista vazia em caso de erro
       }
     };
 
     fetchGenres();
-  }, [])
+  }, []);
 
   return (
     <div>
@@ -66,10 +66,10 @@ export default async function Home({ searchParams }) {
             <GenreButton key={genre.id} genreId={genre.id} genreName={genre.name} />
           ))
         ) : (
-          <p>Carregando gêneros...</p>
-        )};
+          <p>Carregando gêneros...</p> // Ou outra mensagem de carregamento
+        )}
       </div>
-      <Results results={results} genres={genresList} currentPage={currentPage} totalPages={totalPages}/>
+      <Results results={results} genres={genresList} currentPage={currentPage} totalPages={totalPages} setCurrentPage={setCurrentPage} />
     </div>
-  )
+  );
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,46 +1,42 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import Card from "./Card";
 
-export default function Results({results, genres, currentPage, totalPages}) {
-    const handlePreviousPage = () => {
-        if (currentPage > 1){
-            setCurrentPage(currentPage - 1);
-        }
-    };
+export default function Results({ results, genres, currentPage, totalPages, setCurrentPage }) {
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
-    const handleNextPage = () => {
-        if (currentPage < totalPages){
-            setCurrentPage(currentPage + 1)
-        }
-    };
-    
-    return (
-        <div className="max-w-6x1 mx-auto p-4 w-full">
-            <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xlg:grid-cols-5 max-w-6xl mx-auto p-4 w-f">
-                {results && results.length > 0 ? (
-                    results.map((result) => (
-                        <Card key={result.id} result={result} genres={genres} />
-                    ))
-                ) : (
-                    <p>Carregando resultados...</p>
-                )}
-            </div>
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-            <div className="flex justify-center items-center my-5">
-                <Link href={`/?page=${currentPage - 1}`} legacyBehavior>
-                    <button disabled={currentPage <= 1} className="px-4 py-2 bg-orange-500 text-white rounded disabled:opacity-50">
-                        Anterior
-                    </button>
-                </Link>
-                <span className="mx-4">Página {currentPage} de {totalPages}</span>
-                <Link href={`/?page=${currentPage + 1}`} legacyBehavior>
-                    <button disabled={currentPage >= totalPages} className="px-4 py-2 bg-orange-500 text-white rounded disabled:opacity-50">
-                        Próxima
-                    </button>
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className="max-w-6x1 mx-auto p-4 w-full">
+      <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xlg:grid-cols-5 max-w-6xl mx-auto p-4 w-f">
+        {results && results.length > 0 ? (
+          results.map((result) => (
+            <Card key={result.id} result={result} genres={genres} />
+          ))
+        ) : (
+          <p>Carregando resultados...</p> // Ou outra mensagem de carregamento
+        )}
+      </div>
+
+      <div className="flex justify-center items-center my-5">
+        <button onClick={handlePreviousPage} disabled={currentPage <= 1} className="px-4 py-2 bg-orange-500 text-white rounded disabled:opacity-50">
+          Anterior
+        </button>
+        <span className="mx-4">Página {currentPage} de {totalPages}</span>
+        <button onClick={handleNextPage} disabled={currentPage >= totalPages} className="px-4 py-2 bg-orange-500 text-white rounded disabled:opacity-50">
+          Próxima
+        </button>
+      </div>
+    </div>
+  );
 }
